37a03666f585bf84b9200d196340ee95defd93ad
Enzo Cañete <enzodev4@gmail.com>
2024-03-02 16:37:34 -0300

    script V3
    Now it can create the main folder if it doesn't exists

--- script.py
+++ script.py
@@ -1,67 +1,66 @@
 import os, shutil
 
+
+def create_files(folder_path):
+    # 1 Creates a list that contains the names of the files
+    file_list = [
+        "Iconos",
+        "Logos",
+        "Imágenes",
+        "Ideas y Ejemplos",
+        "Fuentes.txt",
+        "Textos.txt",
+    ]
+
+    # A list for the name of the subfolders
+    subfolders_list = ["Vectores", "Fondos", "Overlays"]
+
+    # 2 Loop the list and:
+    for file in file_list:
+        # 3 For each item in the list create a file path
+        file_path = folder_path + "\\" + file
+
+        # 4 If the file doesn't exists create it and move it to the folder and:
+        if not os.path.exists(file_path):
+
+            # 5 If the file_path ends with .txt create the file
+            if file_path.endswith(".txt"):
+                with open(file_path, "w") as fp:
+                    pass
+
+            # 6 Else if the file path ends with "Imagenes",
+            # create three subfolders and move them inside
+            elif file_path.endswith("Imágenes"):
+                os.mkdir(file_path)
+
+                for subfolder in subfolders_list:
+                    os.mkdir(subfolder)
+                    shutil.move(subfolder, file_path)
+
+            # 7 Else create a folder
+            else:
+                os.mkdir(file_path)
+
+        # 8 Else if the file exist, check the next one
+        else:
+            pass
+
+
 while True:
     try:
-        # 1 Ask for the path to a folder
         folder_path = input("Dirección de la carpeta: ")
-
-        # 2 Create a list that contains the names of the files
-        file_list = [
-            "Iconos",
-            "Logos",
-            "Imágenes",
-            "Ideas y Ejemplos",
-            "Fuentes.txt",
-            "Textos.txt",
-        ]
-
-        subfolders_list = ["Vectores", "Fondos", "Overlays"]
-
-        # 3 Loop the list and:
-        for file in file_list:
-            # 4 For each item in the list create a file path
-            file_path = folder_path + "\\" + file
-
-            # 5 If the file doesn't exists create it and move it to the folder and:
-            if not os.path.exists(file_path):
-
-                # 6 If the file_path ends with .txt create the file
-                if file_path.endswith(".txt"):
-                    with open(file_path, "w") as fp:
-                        pass
-
-                # 7 Else if the file path ends with "Imagenes",
-                # create three subfolders and move them inside
-                elif file_path.endswith("Imágenes"):
-                    os.mkdir(file_path)
-                    
-                    for subfolder in subfolders_list:
-                        os.mkdir(subfolder)
-                        shutil.move(subfolder, file_path)
-
-                # 8 Else create a folder
-                else:
-                    os.mkdir(file_path)
-
-            # 9 Else, check the next one
-            else:
-                pass
+        create_files(folder_path)
+        print("\nCarpetas creadas con exito!\n")
 
         break
     except FileNotFoundError:
-        print("La dirección es incorrecta o no existe.")
-
-
-# MVP:
-# + Ask for a path to an existent folder.
-# + Create 7 different folders and 2 .txt files.
-# + The organization should be the next:
-# 	+ 📁 Iconos
-# 	+ 📁Logos
-# 	+ 📁Imágenes
-# 		+ 📁 Vectores
-# 		+ 📁 Fondos
-# 		+ 📁 Overlays
-# 	 + 📁 Ideas y Ejemplos
-# 	 + 📄 Fuentes.txt
-# 	 + 📄Textos.txt
+        print("\nLa dirección es incorrecta o no existe.\n")
+        answer = input("\nQuieres crearla?[si/no]: \n").lower()
+        if answer == "si":
+            os.mkdir(folder_path)
+            create_files(folder_path)
+            print("\ncarpeta creadas con exito!\n")
+            break
+        else:
+            print("\nLa carpeta no sera creada.\n")
+            break
